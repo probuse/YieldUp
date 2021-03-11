07aeaeb17e27c836d476c89b26d45811315aa924
probuse <nprobuse@gmail.com>
2021-03-11 20:10:31 +0300

    add navigateToPageAndRemoveAllPreviousPages to navigation file

--- lib/app/shared/navigation.dart
+++ lib/app/shared/navigation.dart
@@ -3,3 +3,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 Future navigateToPage(String page, {dynamic arguments}) async {
   await Modular.to.pushNamed(page, arguments: arguments);
 }
+
+Future navigateToPageAndRemoveAllPreviousPages(String page,
+    {dynamic arguments}) async {
+  await Modular.to
+      .pushNamedAndRemoveUntil(page, (_) => false, arguments: arguments);
+}
