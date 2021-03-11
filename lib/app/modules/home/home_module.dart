a8c6504695ada40055d7e33c913db7a15c6d0cef
probuse <nprobuse@gmail.com>
2021-03-11 20:07:44 +0300

    bind ResultsController into HomeModule

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:yieldup/app/modules/home/home_controller.dart';
 import 'package:yieldup/app/modules/home/home_page.dart';
+import 'package:yieldup/app/modules/home/results/results_controller.dart';
 import 'package:yieldup/app/modules/home/services/image_picker_service.dart';
 import 'package:yieldup/app/modules/home/services/image_picker_service_interface.dart';
 import 'package:yieldup/app/modules/home/stores/images_store.dart';
@@ -11,6 +12,7 @@ class HomeModule extends ChildModule {
         Bind<IImagePickerService>((i) => ImagePickerService()),
         Bind<ImagesStore>((i) => ImagesStore(i.get<IImagePickerService>())),
         Bind((i) => HomeController(i.get<ImagesStore>())),
+        Bind((i) => ResultsController(i.get<ImagesStore>())),
       ];
 
   @override
